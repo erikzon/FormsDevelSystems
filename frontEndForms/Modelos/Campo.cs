6b24797bf7358953fd81f22a857e50e4985df30c
ErikZon Oliva Del Cid <ericksonoliva@hotmail.com>
2024-02-22 18:42:03 -0600

    [ADD] modelos para front end

---
+++ frontEndForms/Modelos/Campo.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace frontEndForms.Modelos
+{
+    public class Campo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdCampo { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Titulo { get; set; }
+
+        [Required]
+        public bool Requerido { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string TipoCampo { get; set; }
+
+        public Guid IdEncuesta { get; set; }
+
+        [ForeignKey("IdEncuesta")]
+        public Encuesta Encuesta { get; set; }
+
+        public List<Respuesta> Respuestas { get; set; }
+    }
+}
---
+++ frontEndForms/Modelos/Encuesta.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace frontEndForms.Modelos
+{
+    public class Encuesta
+    {
+        [Key]
+        public Guid IdEncuesta { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Descripcion { get; set; }
+
+        public List<Campo> Campos { get; set; }
+    }
+}
